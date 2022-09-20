69f5741aa03cc0b196546f5c4a706b008605d2a2
goncalo-leal <goncalolealsilva@ua.pt>
2022-09-20 15:37:58 +0100

    better interface and word export

--- scraper.py
+++ scraper.py
@@ -3,6 +3,29 @@ import requests
 import urllib.request
 from bs4 import BeautifulSoup
 
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+
+    copiado de https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
 url = 'https://essenciadapoesia.wordpress.com/'
 
@@ -15,10 +38,15 @@ main_bd = {}
 categories_division = {}
 date_division = {}
 
+i = 0
+l = len(archives)
+printProgressBar(i, l, prefix = 'Saving Poems:', suffix = 'Complete', length = 50)
 for archive in archives:
     # temos a certeza que a data só aparece uma vez, por isso não é preciso verificar se já existe no dicionário (para já)
     date = archive.text
-    print(date)
+    
+    i += 1
+    printProgressBar(i, l, prefix = 'Scrapping Poems:', suffix = date + "     ", length = 50)
 
     url = archive["href"]
     r = requests.get(url, headers = headers)
@@ -36,8 +64,10 @@ for archive in archives:
         poem_lines = ""
         for p in content.find_all('p'):
             poem_lines += p.text
-            for br in p.find_all("br"):
+
+            if p.find_all("br").__len__() > 1:
                 poem_lines += '\n'
+
             poem_lines += '\n'
 
         img = content.find('img')
@@ -83,15 +113,19 @@ cats = json.dumps(categories_division, indent=4, sort_keys=True).encode('utf-8')
 dates_file.write(dates)
 cats_file.write(cats)
 
+dates_file.close()
+cats_file.close()
+
+l = len(main_bd.keys())
+printProgressBar(0, l, prefix = 'Saving Poems:', suffix = 'Complete', length = 50)
+i = 0
 for poem in main_bd.keys():
     name = poem.replace(' ', '_')
 
     main_bd[poem]['poem_file'] = name + ".txt"
     f = open("poems/" + name + ".txt", 'w')
-    f.write(poem)
-    f.write('\n')
-    f.write('\n')
-    f.write(main_bd[poem]['poem'])
+    f.write(poem.strip() + '\n\n')
+    f.write(main_bd[poem]['poem'].strip())
     f.close()
 
     if main_bd[poem]['img_url']:
@@ -100,10 +134,10 @@ for poem in main_bd.keys():
     else:
         main_bd[poem]['img_file'] = None
 
+    i += 1
+
+    printProgressBar(i, l, prefix = 'Saving Poems:', suffix = 'Complete', length = 50)
 
 main = json.dumps(main_bd, indent=4, sort_keys=True).encode('utf-8')
 main_file.write(main)
-
-dates_file.close()
-cats_file.close()
 main_file.close()
--- selector.py
+++ selector.py
@@ -1,5 +1,9 @@
+from copy import copy
+import sys
 import json
+import getopt
 import shutil, os
+from docx import Document
 from datetime import datetime
 
 months = {
@@ -17,39 +21,109 @@ months = {
     "Dezembro": "December"
 }
 
-f = open('./main.json')
-poems = json.load(f)
+def get_total():
+    f = open('./main.json')
+    poems = json.load(f)
+    f.close()
 
-new_dict = {}
-for poem in poems:
-    print(poems[poem]["date_division"])
+    print(f"Total: {len(poems)}")
 
-    date_time_str = poems[poem]["date_division"]
-    date = date_time_str.split(" ")
-    print(months[date[0]] + " " + date[1] + " " + date[2])
-    date_time_obj = datetime.strptime(months[date[0]] + " " + date[1] + " " + date[2], '%B %d, %Y').timestamp()
+def order_by_date(poems):
+    print("Ordering by date...")
 
-    new_dict[poem] = [int(round(date_time_obj)), poems[poem]["poem_file"]]
+    new_dict = {}
+    for poem in poems:
+        date_time_str = poems[poem]["date_division"]
+        date = date_time_str.split(" ")
+        date_time_obj = datetime.strptime(months[date[0]] + " " + date[1] + " " + date[2], '%B %d, %Y').timestamp()
 
-f.close()
+        new_dict[poem] = [int(round(date_time_obj)), poems[poem]["poem_file"]]
 
-sorted_dict = {k: v for k, v in sorted(new_dict.items(), key=lambda item: item[1][0])}
+    sorted_dict = {k: v for k, v in sorted(new_dict.items(), key=lambda item: item[1][0])}
 
-to_save = {}
-i = 1
-for poem in sorted_dict.keys():
-    to_save[poem] = {
-        'name': poem,
-        'date': poems[poem]["date_division"],
-        'file': poems[poem]["poem_file"]
-    }
+    return sorted_dict
 
-    shutil.copy(f"./poems/{poems[poem]['poem_file']}", "vol1")
-    
-    if i == 109:
-        break
+def copy_to_destiny(poems, destiny):
+    print("Copying files to destiny directory...")
 
-    i += 1
+    for poem in poems:
+        shutil.copy(f"./poems/{poems[poem]['file']}", destiny)
 
-with open("sorted.json", "w") as write_file:
-    json.dump(to_save, write_file, indent=4, ensure_ascii=False)
+def create_word_file(poems, destiny):
+    print("Creating word file...")
+
+    document = Document()
+    for poem in poems:            
+        with open(f"./poems/{poems[poem]['file']}", "r") as read_file:
+            document.add_paragraph(read_file.read().strip() + "\n\n\n")
+        document.add_page_break()
+
+    document.save(f'{destiny}.docx')
+
+def main(argv):
+    output_dir = ''
+    n = 0
+    try:
+        opts, args = getopt.getopt(argv,"htn:d:",["n=","dir=", "help", "total"])
+    except getopt.GetoptError:
+        print('selector.py -n <number of poems> -d <output directory>')
+        sys.exit(2)
+
+    if len(opts) == 0:
+        print('selector.py -n <number of poems> -d <output directory>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('selector.py -n <number of poems> -d <output directory>')
+            sys.exit()
+
+        elif opt in ("-n", "--n"):
+            if not arg.isdigit():
+                print("Number of poems must be a number")
+                sys.exit(2)
+            n = int(arg)
+
+        elif opt in ("-d", "--dir"):
+            output_dir = arg
+            
+        elif opt in ("-t", "--total"):
+            get_total()
+            sys.exit(0)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    f = open('./main.json')
+    poems = json.load(f)
+    f.close()
+
+    sorted_dict = order_by_date(poems)
+
+    print(type(n))
+
+    to_save = {}
+    i = 1
+    for poem in sorted_dict.keys():
+        to_save[poem] = {
+            'name': poem,
+            'date': poems[poem]["date_division"],
+            'file': poems[poem]["poem_file"]
+        }
+        
+        if i == n:
+            print(i)
+            break
+
+        i += 1
+    print(i)
+
+    copy_to_destiny(to_save, output_dir)
+
+    with open("sorted.json", "w") as write_file:
+        json.dump(to_save, write_file, indent=4, ensure_ascii=False)
+
+    create_word_file(to_save, output_dir)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
